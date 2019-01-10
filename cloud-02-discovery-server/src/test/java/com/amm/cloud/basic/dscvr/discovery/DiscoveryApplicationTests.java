af12d97f80de9186ea90d2d168ee62ce66d8bf14
amartinm7 <amartinm7@gmail.com>
2019-01-10 23:01:04 +0100

    first commit, adding config server, discovery server, gateway server

---
+++ cloud-02-discovery-server/src/test/java/com/amm/cloud/basic/dscvr/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,17 @@
+package com.amm.cloud.basic.dscvr.discovery;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DiscoveryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
